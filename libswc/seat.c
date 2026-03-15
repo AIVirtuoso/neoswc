2d53c6d23e3f63ddc72335cc61000f2f05830beb
muckywaffles <waffles@mucky.me>
2026-03-15 02:36:48 +0000

    Fix null release request in wl_seat
    This was apparently missed when wl_seat was update from version 4 to 8.
    It causes the compositor to crash when closing a client that calls it. In this case foot...

--- libswc/seat.c
+++ libswc/seat.c
@@ -160,9 +160,9 @@ get_touch(struct wl_client *client, struct wl_resource *resource, uint32_t id)
 }
 
 static const struct wl_seat_interface seat_impl = {
-    .get_pointer = get_pointer,
-    .get_keyboard = get_keyboard,
-    .get_touch = get_touch,
+	.get_pointer = get_pointer,
+	.get_keyboard = get_keyboard,
+	.get_touch = get_touch,
 	.release = destroy_resource,
 };
 
